057d4d97d1ae17ad6a6e20d58843711b669f168f
Jayant Singh <jayantofficial1305@gmail.com>
2026-03-30 18:53:48 +0530

    DAY 3 : ARRAY GENERATION FUNCTION

---
+++ DAY 3/array_generation_function.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+print(arr)
+
+print("------")
+
+arr1 = np.zeros(5)
+print(arr1)
+
+print("------")
+
+arr2 = np.zeros((2,3))
+print(arr2)
+
+print("------")
+
+arr3 = np.ones(5)
+print(arr3)
+
+print("------")
+
+arr4 = np.ones((2,3))
+print(arr4)
+
+print("------")
+
+arr5 = np.empty(5)
+print(arr5)
+
+print("------")
+
+arr6 = np.arange(1,10,2)
+print(arr6)
+
+print("------")
+
+arr7 = np.linspace(0,10,5)
+print(arr7)
+
+print("------")
+
+arr8 = np.random.rand(3)
+print(arr8)
+
+print("------")
+
+arr9 = np.random.randn(5)
+print(arr9)
+
+print("------")
+
+arr10 = np.random.randint(1,10,5)
+print(arr10)
+
+print("------")
+
+arr11 = np.eye(3)
+print(arr11)
+
+print("------")
+
+arr12 = np.identity(3)
+print(arr12)
+
+print("------")
+
+arr13 = np.full((2,3),5)
+print(arr13)
+
+print("------")
