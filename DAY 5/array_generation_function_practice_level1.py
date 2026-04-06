8ee6f7c174eb2061bd90b6cdae95c80f5701b860
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-06 14:08:11 +0530

    DAY 5 : ARRAY GENERATION FUNCTION

---
+++ DAY 5/array_generation_function_practice_level1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.zeros(10)
+print(arr1)
+
+print("------")
+
+arr2 = np.linspace(0,10,5)
+print(arr2)
+
+print("------")
+
+arr3 = np.full((3,3),7)
+print(arr3)
---
+++ DAY 5/array_generation_function_practice_level2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr1 = np.arange(0,9,2)
+print(arr1)
+
+print("------")
+
+arr2 = np.eye(4)
+print(arr2)
