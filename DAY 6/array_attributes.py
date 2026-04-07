adcd6ecbb709006b6c2cd1e18646addf49381f81
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-07 15:23:08 +0530

    DAY 6 : ARRAY ATTRIBUTES

---
+++ DAY 6/array_attributes.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# arr = np.array([[1,2,3],[4,5,6]])
+
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+
+# arr = np.array([1.2,2.5,3.7])
+# print(arr.dtype)
+
+# arr = np.array([1,2,3,4,5])
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+
+# arr = np.array([[1,2],[3,4],[5,6]])
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+
+# arr = np.array([1.2,3.5,6.7,8.9])
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+
+data = np.array([[23,45,67],[34,65,78],[12,34,56],[67,89,90]])
+print(data.shape)
+print(data.size)
+print(data.dtype)
---
+++ DAY 6/array_attributes_Level1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.array([10,20,30,40])
+
+print(arr.shape)
+print(arr.size)
+print(arr.dtype)
+
+print("------")
+
+arr1 = np.array([[1,2,3],[4,5,6]])
+print(arr1.shape)
+print(arr1.size)
+print(arr1.dtype)
+
+print("------")
+
+arr2 = np.array([1.5,2.3,4.6])
+print(arr2.shape)
+print(arr2.size)
+print(arr2.dtype)
