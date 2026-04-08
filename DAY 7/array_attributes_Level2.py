00049eaa2894649265b2cdcf944deca4892ba59c
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-08 15:06:50 +0530

    DAY 7 : ARRAY METHODS

---
+++ DAY 7/array_attributes_Level2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([[1,2],[3,4],[5,6]])
+print(arr.shape)
+print(arr.size)
+print(arr.dtype)
+
+print("------")
+
+data = np.array([[23,45,67],[34,65,78],[12,34,56],[67,89,90]])
+print(data.shape)
+print(data.size)
+print(data.dtype)
