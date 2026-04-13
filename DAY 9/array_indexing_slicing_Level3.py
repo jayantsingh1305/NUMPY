066f6549722289bc48bb2d4cb058d5b7beb3a2bb
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-13 17:00:30 +0530

    DAY 9 : ARRAY INDEXING AND SLICING

---
+++ DAY 9/array_indexing_slicing_Level3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+salary = np.array([25000, 30000, 35000, 40000, 45000, 50000])
+
+print(salary[0:3])
+print(salary[-1:-3:-1])
+print(salary[::2])
---
+++ DAY 9/array_indexing_slicing_Level4.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+data = np.array([10,20,30,40,50,60,70,80,90,100])
+
+print(data[0:6])
+print(data[7:9])
+print(data[8:])
