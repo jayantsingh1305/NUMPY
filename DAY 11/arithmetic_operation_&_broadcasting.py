3201fcc63e8d858275689afc406bc2471ae27e7a
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-20 16:26:27 +0530

    DAY 11 : ARITHMETIC OPERATION & BROADCASTING

---
+++ DAY 11/arithmetic_operation_&_broadcasting.py
@@ -0,0 +1,33 @@
+import numpy as np
+# a = np.array([10,20,30])
+# b = np.array([1,2,3])
+
+# print(a+b)
+# print(a*b)
+# print(a+5)
+
+# a = np.array([[1,2,3],[4,5,6]])
+# b = np.array([10,20,30])
+# print(a+b)
+
+a = np.array([2,4,6])
+b = np.array([1,3,5])
+print(a-b)
+
+print("------")
+
+a = np.array([[1,2],[3,4]])
+b = 10
+print(a+b)
+
+print("------")
+
+a = np.array([[1,2,3]])
+b = np.array([[1],[2]])
+print(a+b)
+
+print("------")
+
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([1,2,3])
+print(a+b)
---
+++ DAY 11/arithmetic_operation_&_broadcasting_Level1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# a = np.array([2,4,6])
+# b = np.array([1,3,5])
+
+# result = a + b
+# print(result)
+
+# a= np.array([10,20,30])
+# result=(a*2)
+# print(result)
+
+a = np.array([[1,2],[3,4]])
+b = 10
+print(a+b)
