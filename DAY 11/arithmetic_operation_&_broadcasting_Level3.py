00415fc8989a15eff65fac7f4df8f5df0caec243
Jayant Singh <jayantofficial1305@gmail.com>
2026-04-20 16:27:09 +0530

    DAY 11 : ARITHMETIC OPERATION & BROADCASTING

---
+++ DAY 11/arithmetic_operation_&_broadcasting_Level3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# x = np.array([[10,20,30],[40,50,60]])
+# mean = np.array([25,35,45])
+# result = x - mean
+# print(result)
+
+# x = np.array([[1,2,3],[4,5,6]])
+# w = np.array([1,1,1])
+# b = 10
+# z = x * w + b
+# print(z)
+
+image = np.array([[100, 150], [200, 250]])
+brightness = 50
+
+print(image + brightness)
